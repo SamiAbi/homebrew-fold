93404dc4d90cc18978e555e953eef4ac135ec6ce
Sami Abishai <39597229+SamiAbi@users.noreply.github.com>
2026-08-19 23:07:21 +0300

    fold v0.32.0

--- Casks/fold.rb
+++ Casks/fold.rb
@@ -1,6 +1,6 @@
 cask "fold" do
-  version "0.31.0"
-  sha256 "91913eeab174f34d4a2de71489bbd98e57fb09c5d2c0e7bb4785dfb53b0d4674"
+  version "0.32.0"
+  sha256 "b041d28ef46942e2424f95938278d3a9b0d7e70c305f191ed1c9ef9aa6be05c7"
 
   url "https://github.com/SamiAbi/fold-dist/releases/download/v#{version}/Fold.dmg"
   name "Fold"
